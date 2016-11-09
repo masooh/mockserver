bf3737e5e59c4c0b384a35f71ac17a3e684e9d6b
Martin <martin_hofmann_de@yahoo.de>
2016-11-09 22:02:54 +0100

    Rewrite NettyHttpClientTest in Spock

--- mockserver-core/src/test/groovy/org/mockserver/client/netty/NettyHttpClientTest.groovy
+++ mockserver-core/src/test/groovy/org/mockserver/client/netty/NettyHttpClientTest.groovy
@@ -1,54 +1,43 @@
-package org.mockserver.client.netty;
+package org.mockserver.client.netty
 
-import com.google.common.net.MediaType;
-import io.netty.handler.codec.http.HttpHeaders;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
-import org.mockserver.echo.http.EchoServer;
-import org.mockserver.model.HttpResponse;
-import org.mockserver.socket.PortFactory;
+import io.netty.handler.codec.http.HttpHeaders
+import org.mockserver.echo.http.EchoServer
+import org.mockserver.model.HttpResponse
+import org.mockserver.socket.PortFactory
+import spock.lang.Specification
 
-import static io.netty.handler.codec.http.HttpHeaders.Names.*;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.core.Is.is;
-import static org.mockserver.model.Cookie.cookie;
-import static org.mockserver.model.Header.header;
-import static org.mockserver.model.HttpRequest.request;
-import static org.mockserver.model.HttpResponse.response;
-import static org.mockserver.model.OutboundHttpRequest.outboundRequest;
-import static org.mockserver.model.StringBody.exact;
+import static io.netty.handler.codec.http.HttpHeaders.Names.*
+import static org.hamcrest.MatcherAssert.assertThat
+import static org.hamcrest.core.Is.is
+import static org.mockserver.model.Cookie.cookie
+import static org.mockserver.model.Header.header
+import static org.mockserver.model.HttpRequest.request
+import static org.mockserver.model.HttpResponse.response
+import static org.mockserver.model.OutboundHttpRequest.outboundRequest
+import static org.mockserver.model.StringBody.exact
 
-public class NettyHttpClientTest {
+public class NettyHttpClientTest extends Specification {
 
-    private static EchoServer echoServer;
-    private static int freePort;
-    @Rule
-    public ExpectedException exception = ExpectedException.none();
+    private static EchoServer echoServer
+    private static int freePort
 
-    @BeforeClass
-    public static void startEchoServer() {
-        freePort = PortFactory.findFreePort();
-        echoServer = new EchoServer(freePort, false);
+    def setupSpec() {
+        freePort = PortFactory.findFreePort()
+        echoServer = new EchoServer(freePort, false)
     }
 
-    @AfterClass
-    public static void stopEchoServer() {
-        echoServer.stop();
+    def cleanupSpec() {
+        echoServer.stop()
     }
 
-    @Test
-    public void shouldSendBasicRequest() {
-        // given
-        NettyHttpClient nettyHttpClient = new NettyHttpClient();
+    def "should send basic request"() {
+        given:
+        NettyHttpClient nettyHttpClient = new NettyHttpClient()
 
-        // when
-        HttpResponse httpResponse = nettyHttpClient.sendRequest(outboundRequest("0.0.0.0", freePort, "", request()));
+        when:
+        HttpResponse httpResponse = nettyHttpClient.sendRequest(outboundRequest("0.0.0.0", freePort, "", request()))
 
-        // then
+        then:
         assertThat(httpResponse, is(
                 response()
                         .withStatusCode(200)
@@ -56,15 +45,14 @@ public class NettyHttpClientTest {
                         .withHeader(header(CONTENT_LENGTH, 0))
                         .withHeader(header(ACCEPT_ENCODING, HttpHeaders.Values.GZIP + "," + HttpHeaders.Values.DEFLATE))
                         .withHeader(header(CONNECTION, HttpHeaders.Values.KEEP_ALIVE))
-        ));
+        ))
     }
 
-    @Test
-    public void shouldSendComplexRequest() {
-        // given
-        NettyHttpClient nettyHttpClient = new NettyHttpClient();
+    def "should send complex request"() {
+        given:
+        NettyHttpClient nettyHttpClient = new NettyHttpClient()
 
-        // when
+        when:
         HttpResponse httpResponse = nettyHttpClient.sendRequest(outboundRequest("0.0.0.0", freePort, "",
                 request()
                         .withHeader(header("some_header_name", "some_header_value"))
@@ -72,9 +60,9 @@ public class NettyHttpClientTest {
                         .withCookie(cookie("some_cookie_name", "some_cookie_value"))
                         .withCookie(cookie("another_cookie_name", "another_cookie_value"))
                         .withBody(exact("this is an example body"))
-        ));
+        ))
 
-        // then
+        then:
         assertThat(httpResponse, is(
                 response()
                         .withStatusCode(200)
@@ -88,7 +76,7 @@ public class NettyHttpClientTest {
                         .withCookie(cookie("some_cookie_name", "some_cookie_value"))
                         .withCookie(cookie("another_cookie_name", "another_cookie_value"))
                         .withBody(exact("this is an example body"))
-        ));
+        ))
     }
 
 }
