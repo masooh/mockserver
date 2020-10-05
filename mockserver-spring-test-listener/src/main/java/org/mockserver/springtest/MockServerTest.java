4bc88eca581913beb1a113206f6a45500c74de85
Martin Hofmann-Sobik <martin.hofmannsobik@1und1.de>
2020-10-05 10:11:29 +0200

    #767: Delegate to TestPropertySource with AliasFor

--- mockserver-spring-test-listener/src/main/java/org/mockserver/springtest/MockServerTest.java
+++ mockserver-spring-test-listener/src/main/java/org/mockserver/springtest/MockServerTest.java
@@ -1,10 +1,16 @@
 package org.mockserver.springtest;
 
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.test.context.TestPropertySource;
+
 import java.lang.annotation.*;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
+@TestPropertySource
 @Inherited
 public @interface MockServerTest {
-    String[] value() default {};
+
+    @AliasFor(annotation = TestPropertySource.class, attribute = "properties")
+    String[] value() default {""};
 }
--- mockserver-spring-test-listener/src/main/java/org/mockserver/springtest/MockServerTestCustomizerFactory.java
+++ mockserver-spring-test-listener/src/main/java/org/mockserver/springtest/MockServerTestCustomizerFactory.java
@@ -5,12 +5,14 @@ import org.springframework.test.context.ContextConfigurationAttributes;
 import org.springframework.test.context.ContextCustomizer;
 import org.springframework.test.context.ContextCustomizerFactory;
 
+import javax.annotation.Nullable;
 import java.util.List;
 
 public class MockServerTestCustomizerFactory implements ContextCustomizerFactory {
     @Override
+    @Nullable
     public ContextCustomizer createContextCustomizer(Class<?> testClass, List<ContextConfigurationAttributes> configAttributes) {
         final MockServerTest mockServerTestAnnotation = AnnotatedElementUtils.findMergedAnnotation(testClass, MockServerTest.class);
-        return mockServerTestAnnotation != null ? new MockServerPropertyCustomizer(mockServerTestAnnotation.value()) : null;
+        return mockServerTestAnnotation != null ? new MockServerPropertyCustomizer() : null;
     }
 }
