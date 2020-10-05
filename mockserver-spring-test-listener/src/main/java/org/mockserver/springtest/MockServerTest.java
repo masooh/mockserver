0021e81a1c0ae5333583cb3ea142a185028ebaab
Martin Hofmann-Sobik <martin.hofmannsobik@1und1.de>
2020-10-05 10:11:29 +0200

    #767: Delegate to TestPropertySource with AliasFor

--- mockserver-spring-test-listener/src/main/java/org/mockserver/springtest/MockServerTest.java
+++ mockserver-spring-test-listener/src/main/java/org/mockserver/springtest/MockServerTest.java
@@ -1,10 +1,20 @@
 package org.mockserver.springtest;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.test.context.TestPropertySource;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
+@TestPropertySource
 @Inherited
 public @interface MockServerTest {
-    String[] value() default {};
+
+    @AliasFor(annotation = TestPropertySource.class, attribute = "properties")
+    String[] value() default { "" };
 }
